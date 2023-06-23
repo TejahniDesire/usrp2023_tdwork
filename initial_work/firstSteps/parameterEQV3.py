1c87114a44f574297cd357d6b9ab27b7ecbf0faf
Tejahni Desire <td6241@princeton.edu>
2023-06-23 13:08:16 -0400

    Worked on parameters as a function of r

--- initial_work/firstSteps/parameterEQV3.py
+++ initial_work/firstSteps/parameterEQV3.py
@@ -2,23 +2,95 @@ import numpy as np
 from astropy import constants as const
 from astropy import units as u
 
+# All r in units of Rg
 
-def nth_func(r, n_th0, p, mass):
-    rb = rb_func(mass)
-    return n_th0 * (r/rb) ** p
+G = const.G.cgs
+c = const.c.cgs
+me = const.m_e.cgs
+mp = const.m_p.cgs
+e = const.e.esu
+kB = const.k_B.cgs
 
 
-def te_func(r, t_e0, p, mass):
-    rb = rb_func(mass)
-    return t_e0 * (r/rb) ** p
-
-
-def b_func(r, n_th0, p, mass, beta):
-    if n_th0.unit != u.cm ** -3:
-        raise ValueError('n_th0 needs units of number density')
-    nth = nth_func(r*u.cm, n_th0, p, mass)
-    return np.sqrt(nth * 8 * np.pi * const.m_p.cgs * const.c.cgs ** 2 / (6 * beta * r))
+def rg_func(mass):
+    return G * mass / c ** 2
 
 
 def rb_func(mass):
-    return 20 * const.G.cgs * mass / (const.c.cgs ** 2)
+    return 20 * G * mass / (c ** 2)
+
+
+def size_func(r, mass, scale_height):
+    rg = rg_func(mass)
+    return r * scale_height * rg
+
+
+def te_func(r, mass, t_e0, p_temp):
+    if t_e0.unit != u.K:
+        raise ValueError('n_th0 needs units of Kelvin')
+    rb = rb_func(mass)
+    rg = rg_func(mass)
+    return t_e0 * (r*rg/rb) ** p_temp
+
+
+def theta_e_func(r, mass, t_e0, p_temp):
+    temp = te_func(r, mass, t_e0, p_temp)
+    return (kB * temp / (me * c ** 2)).to(u.dimensionless_unscaled)
+
+
+def nth_func(r, mass, n_th0, p_dens):
+    if n_th0.unit != u.cm ** -3:
+        raise ValueError('n_th0 needs units of number density')
+    rg = rg_func(mass)
+    rb = rb_func(mass)
+    return n_th0 * (r * rg / rb) ** p_dens
+
+
+def b_func(r, mass, n_th0, p_dens, beta):
+    rg = rg_func(mass)
+    if n_th0.unit != u.cm ** -3:
+        raise ValueError('n_th0 needs units of number density')
+    nth = nth_func(r, mass, n_th0, p_dens)
+    return np.sqrt(nth * 8 * np.pi * mp * c ** 2 / (6 * beta * r))
+
+
+def nu_b_func(r, mass, n_th0, p_dens, beta):
+    b_field = b_func(r, mass, n_th0, p_dens, beta)
+    return (e * b_field / (2*np.pi * me * c)).to(u.Hz)
+
+
+def nu_c_func(r, mass, n_th0, p_dens, beta, theta_b, gamma):
+    nu_b = nu_b_func(r, mass, n_th0, p_dens, beta)
+    return 3/2*nu_b*np.sin(theta_b)*gamma**2
+
+
+def x_func(r, nu, mass, n_th0, p_dens, beta, theta_b, gamma):
+    return nu / nu_c_func(r, mass, n_th0, p_dens, beta, theta_b, gamma)
+
+
+def nu_func(r, x, mass, n_th0, p_dens, beta, theta_b, gamma):
+    return x * nu_c_func(r, mass, n_th0, p_dens, beta, theta_b, gamma)
+
+
+def synchrotron_func(x):
+    return 2.5651 * (1 + 1.92 * (x ** (-1/3)) + (0.9977*x**(-2/3)))*np.exp(-1.8899*x**(1/3))
+
+
+def j_emission_x_func(r, x, mass, n_th0, p_dens, t_e0, p_temp, beta, theta_b, gamma):
+    n = nth_func(r, mass, n_th0, p_dens)
+    theta_e = theta_e_func(r, mass, t_e0, p_temp)
+    nu = nu_func(r, x, mass, n_th0, p_dens, beta, theta_b, gamma)
+    return (n * e ** 2 * nu * synchrotron_func(x) / (2 * np.sqrt(3) * c * theta_e ** 2)).to(u.erg / (u.cm ** 3 * u.s * u.Hz))
+
+
+def specific_intensity(r, x, mass, n_th0, p_dens, t_e0, p_temp, beta, theta_b, gamma, scale_height):  
+    j_coeff = j_emission_x_func(r, x, mass, n_th0, p_dens, t_e0, p_temp, beta, theta_b, gamma)
+    size = size_func(r, mass, scale_height)
+    return (size * j_coeff).to(u.erg / u.cm **2)
+
+
+def bright_temp(r, x, mass, n_th0, p_dens, t_e0, p_temp, beta, theta_b, gamma, scale_height):
+    s_intensity = specific_intensity(r, x, mass, n_th0, p_dens, t_e0, p_temp, beta, theta_b, gamma, scale_height)
+    nu = nu_func(r, x, mass, n_th0, p_dens, beta, theta_b, gamma)
+    return (const.c.cgs ** 2 / (2 * nu ** 2 * const.k_B.cgs) * s_intensity).to(u.K)
+
--- initial_work/firstSteps/tools.py
+++ initial_work/firstSteps/tools.py
@@ -1,9 +1,16 @@
 import matplotlib.pyplot as plt
-
+from astropy import units as u
 
 def plot_with_units(x_values, y_values, label=None, xlabel='' ,ylabel=''):
     plt.plot(x_values, y_values,  label=label)
-    plt.xlabel(str(xlabel)+ " (" + str(x_values[0].unit) + ")")
-    plt.ylabel(str(ylabel)+ " (" + str(y_values[0].unit) + ")")
+    if str(x_values[0].unit) != u.dimensionless_unscaled:
+        plt.xlabel(str(xlabel)+ " (" + str(x_values[0].unit) + ")")
+    else:
+        plt.xlabel(str(xlabel))
+        
+    if str(y_values[0].unit) != u.dimensionless_unscaled:
+        plt.ylabel(str(ylabel)+ " (" + str(y_values[0].unit) + ")")
+    else:
+        plt.ylabel(str(ylabel))
 
 
